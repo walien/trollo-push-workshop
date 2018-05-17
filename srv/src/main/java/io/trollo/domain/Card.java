3612372ac4f70052e0ff8ed28b35f4bba995c1fa
Elian ORIOU <eoriou@gmail.com>
2018-05-17 21:56:41 +0200

    fix id issue

--- srv/src/main/java/io/trollo/domain/Card.java
+++ srv/src/main/java/io/trollo/domain/Card.java
@@ -1,10 +1,12 @@
 package io.trollo.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jongo.marshall.jackson.oid.MongoId;
 
 public class Card implements Payload  {
 
     @MongoId
+    @JsonProperty("_id")
     private String id;
 
     private String title;
