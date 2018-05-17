bc2e4fbf59afe6100c7a9cf1b7deb84814d0826b
Elian ORIOU <eoriou@gmail.com>
2018-05-17 20:59:18 +0200

    publish instant message and cards changes

--- srv/src/main/java/io/trollo/services/CardService.java
+++ srv/src/main/java/io/trollo/services/CardService.java
@@ -1,5 +1,7 @@
 package io.trollo.services;
 
+import io.trollo.broker.Exchange;
+import io.trollo.broker.MQClient;
 import io.trollo.domain.Card;
 import io.trollo.domain.State;
 import restx.factory.Component;
@@ -13,10 +15,12 @@ public class CardService {
 
     private final JongoCollection cards;
     private final SequenceService sequencesService;
+    private MQClient mqClient;
 
-    public CardService(@Named("cards") JongoCollection cards, SequenceService sequencesService) {
+    public CardService(@Named("cards") JongoCollection cards, SequenceService sequencesService, MQClient mqClient) {
         this.cards = cards;
         this.sequencesService = sequencesService;
+        this.mqClient = mqClient;
     }
 
     public Iterable<Card> findAll() {
@@ -30,12 +34,17 @@ public class CardService {
         return card;
     }
 
+    private void publishCardChange(Card card) {
+        mqClient.publish(Exchange.byName("trollo.cards", "topic"), "*", card);
+    }
+
     public Optional<Card> updateCard(String id, Card card) {
         Card dbCard = cards.get()
                 .findAndModify("{ _id: # }", id)
                 .with("{ $set: { title: #, body: # } }", card.getTitle(), card.getBody())
                 .returnNew()
                 .as(Card.class);
+        publishCardChange(dbCard);
         return Optional.ofNullable(dbCard);
     }
 
@@ -45,6 +54,7 @@ public class CardService {
                 .with("{ $set: { state: # } }", state.name())
                 .returnNew()
                 .as(Card.class);
+        publishCardChange(dbCard);
         return Optional.ofNullable(dbCard);
     }
 }
--- srv/src/main/java/io/trollo/services/InstantMessageService.java
+++ srv/src/main/java/io/trollo/services/InstantMessageService.java
@@ -1,5 +1,7 @@
 package io.trollo.services;
 
+import io.trollo.broker.Exchange;
+import io.trollo.broker.MQClient;
 import io.trollo.domain.InstantMessage;
 import restx.factory.Component;
 import restx.jongo.JongoCollection;
@@ -10,9 +12,11 @@ import javax.inject.Named;
 public class InstantMessageService {
 
     private final JongoCollection messages;
+    private final MQClient mqClient;
 
-    public InstantMessageService(@Named("messages") JongoCollection messages) {
+    public InstantMessageService(@Named("messages") JongoCollection messages, MQClient mqClient) {
         this.messages = messages;
+        this.mqClient = mqClient;
     }
 
     public Iterable<InstantMessage> findAll() {
@@ -21,6 +25,7 @@ public class InstantMessageService {
 
     public InstantMessage createMessage(InstantMessage message) {
         messages.get().insert(message);
+        mqClient.publish(Exchange.byName("trollo.messages", "topic"), "*", message);
         return message;
     }
 }
