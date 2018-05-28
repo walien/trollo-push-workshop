492a1a60ed46e68d62932a232ae3003e012b3949
Elian ORIOU <eoriou@gmail.com>
2018-05-28 22:29:57 +0200

    remove useless server side config

--- srv/src/main/java/io/trollo/broker/BrokerSettings.java
+++ srv/src/main/java/io/trollo/broker/BrokerSettings.java
@@ -15,13 +15,4 @@ public interface BrokerSettings {
     @SettingsKey(key = "broker.password", defaultValue = "guest")
     String brokerPassword();
 
-    @SettingsKey(key = "stomp.url", defaultValue = "http://127.0.0.1:15674/stomp")
-    String stompUrl();
-
-    @SettingsKey(key = "stomp.login", defaultValue = "guest")
-    String stompLogin();
-
-    @SettingsKey(key = "stomp.password", defaultValue = "guest")
-    String stompPassword();
-
 }
